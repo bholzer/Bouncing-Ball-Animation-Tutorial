45933fad3cce5ba8dfb201ee2c0cad3feb00cb5d
bholzer <brennan.holzer@complymd.com>
2014-12-02 20:58:24 -0600

    Clean up src

--- lesson_3/src/ball_animation/scripts/application.js
+++ lesson_3/src/ball_animation/scripts/application.js
@@ -5,10 +5,7 @@ window.onload = function() {
   context.canvas.width = window.innerWidth;
   context.canvas.height = window.innerHeight;
 
-
-  // Here is our ball object, with its initial state
-  // We define it outside of the render function because if it were inside of the function, it would reset every time a new frame rendered.
-  // That's a bad thing, because this object is going to keep track of the important data!
+  // The ball object that will keep track of the ball's state
   var ball = {
     x: 0,
     y: 0,
@@ -19,22 +16,20 @@ window.onload = function() {
     // Step 1: Clear existing shapes
     context.clearRect(0, 0, canvas.width, canvas.height);
 
-    // Step 2: Draw and make transformations
-    // This draw stuff should look familiar
+    // Step 2: Draw shape using its existing data (state)
     context.beginPath();
-    context.arc(ball.x, ball.y, ball.radius, 0, Math.PI*2); // Notice here that the (x,y) coordinates come from the ball object
+    context.arc(ball.x, ball.y, ball.radius, 0, Math.PI*2); // Notice here that the (x,y) coordinates and radius come from the ball object
     context.closePath();
     context.fill();
 
-    // This will be our transformation
-    // This changes the ball object so that its x and y coordinates are now one more than they were before
-    // This makes it so that when the next frame is drawn, the circle is drawn in a new position
+    // Step 3: Transform the shape's data to make it move diagonally
     ball.x = ball.x + 1;
     ball.y = ball.y + 1;
 
-    // Step 3: Redraw!
+    // Step 4: Repeat!
     window.requestAnimationFrame(render);
   };
 
+  //Finally, call our render function to start the animation
   render();
 }
